1e31f97b018542b996a95ab8c813c10abe9ba59c
Kevin <kevinraihanr45@gmail.com>
2022-11-17 13:18:55 +0700

    Added Account Activation UI

--- Bebas/app/src/main/java/com/example/bebas/AccountActivate.kt
+++ Bebas/app/src/main/java/com/example/bebas/AccountActivate.kt
@@ -3,7 +3,7 @@ package com.example.bebas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AccountActivate : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_activate)
