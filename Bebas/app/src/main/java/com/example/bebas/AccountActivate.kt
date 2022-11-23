42db597b13b7de901c9a115693a2ec7791b3e458
Kevin <kevinraihanr45@gmail.com>
2022-11-23 10:17:27 +0700

    added intent Logic

--- Bebas/app/src/main/java/com/example/bebas/AccountActivate.kt
+++ Bebas/app/src/main/java/com/example/bebas/AccountActivate.kt
@@ -1,11 +1,26 @@
 package com.example.bebas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bebas.databinding.ActivityAccountActivateBinding
+import com.example.bebas.databinding.ActivityMainBinding
 
 class AccountActivate : AppCompatActivity() {
+    private lateinit var binding: ActivityAccountActivateBinding
+    private val txIdUser: String get() = binding.userIdTextInput.editText?.text.toString()
+    private val txNorek: String get() = binding.noRekTextInput.editText?.text.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_activate)
+        binding = ActivityAccountActivateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.userIdTextInput.editText?.setText(intent.getStringExtra("username"))
+
+        binding.toLogin.setOnClickListener {
+            startActivity(Intent(this@AccountActivate, MainActivity::class.java)
+                .putExtra("idUser", txIdUser).putExtra("noRek", txNorek))
+        }
     }
 }
